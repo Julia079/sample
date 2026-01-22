fae9f0b133cd4bd4c1d8e73f178514b3c174e30b
Abby <bautista.julia.abella@gmail.com>
2026-01-22 14:29:26 +0800

    Login to Register and vice versa

--- app/src/main/java/com/example/sample/Login.kt
+++ app/src/main/java/com/example/sample/Login.kt
@@ -1,6 +1,8 @@
 package com.example.sample
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,13 @@ class Login : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val registerTextView: TextView = findViewById(R.id.textViewRegister)
+        registerTextView.setOnClickListener {
+            val intent = Intent(this, Register::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/sample/Register.kt
+++ app/src/main/java/com/example/sample/Register.kt
@@ -1,6 +1,8 @@
 package com.example.sample
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,12 @@ class Register : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val loginTextView: TextView = findViewById(R.id.textViewLogin)
+        loginTextView.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+            startActivity(intent)
         }
     }
 }
